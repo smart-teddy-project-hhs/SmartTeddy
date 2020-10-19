90333c5b22d67ac2d0b392bbab7dcd9ee93249f5
Huub van Holsteijn <hwpvanholsteijn@gmail.com>
2020-10-19 18:31:36 +0200

    Added admin panel lexicons

--- smartteddy.basket.computer/web/smartteddydashboard/speech_recognition/admin.py
+++ smartteddy.basket.computer/web/smartteddydashboard/speech_recognition/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SentimentLemma
+
+
+class SentimentLemmaInline(admin.TabularInline):
+    model = SentimentLemma
+
+
+class SentimentLemmaAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['lemma_written_form', 'sentiment_polarity', 'part_of_speech']}),
+    ]
+    list_display = ('lemma_written_form', 'sentiment_polarity', 'part_of_speech')
+    search_fields = ['lemma_written_form', 'sentiment_polarity', 'part_of_speech']
+    list_filter = ['sentiment_polarity', 'part_of_speech']
+
+
+admin.site.register(SentimentLemma, SentimentLemmaAdmin)
