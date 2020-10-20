9424b43625949295eccc574596122641f2a9ae9e
Huub van Holsteijn <hwpvanholsteijn@gmail.com>
2020-10-21 00:45:08 +0200

    moved url's

--- smartteddy.basket.computer/web/smartteddydashboard/speech_recognition/views.py
+++ smartteddy.basket.computer/web/smartteddydashboard/speech_recognition/views.py
@@ -2,10 +2,6 @@ from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 
 
-def index(request):
-    return 404
-
-
 @require_POST
 def sentence(request):
-    pass
+    return HttpResponse("hello world")
