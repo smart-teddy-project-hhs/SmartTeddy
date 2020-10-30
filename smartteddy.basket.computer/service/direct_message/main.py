d4bcc98522db9d96b353ebb7cda654b810722d25
Huub van Holsteijn <hwpvanholsteijn@gmail.com>
2020-10-30 18:05:47 +0100

    Added simple Twilio API

--- smartteddy.basket.computer/service/direct_message/main.py
+++ smartteddy.basket.computer/service/direct_message/main.py
@@ -1,47 +1,28 @@
 # Source: https://www.twilio.com/docs/conversations/using-whatsapp-conversations
 # Select Python
-# Warning: Twilio is a commercial product and costs money per message
-
-from twilio.rest import Client
+# Warning: Twilio is a commercial product and costs money per message when not unsandboxed
 import environ
+from twilio.rest import Client
+
+environ.Env.read_env()
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    IS_WHATSAPP_BUSINESS=(bool, False)
 )
 
-is_business_whatsapp = False
-account = ""
-token = ""
-phone_from = ""
-phone_to = ""
-twilio_client = Client(account, token, region="NL")
-twilio_client.messages()
+is_business_whatsapp = env('IS_WHATSAPP_BUSINESS')
+account = env('TWILIO_ACCOUNT_SID')
+token = env('TWILIO_AUTH_TOKEN')
+phone_from = env('TWILIO_PHONE_FROM')
+phone_to = env('TWILIO_PHONE_TO')
+twilio_client = Client(account, token)
 
-# create an new conversation with a person
-conversation = twilio_client.conversations.conversations.create()
-conversation_id = conversation.sid
+"""Quickstart"""
+message = twilio_client.messages.create(
+                              body='Smart Teddy is awesome!',
+                              from_=f'whatsapp: {phone_from}',
+                              to=f'whatsapp:{phone_to}'
+                          )
 
-# Add participants to the conversation
-if not is_business_whatsapp:
-    participant = twilio_client.conversations \
-        .conversations(conversation_id) \
-        .participants \
-        .create(messaging_binding_address=phone_from, messaging_binding_proxy_address=phone_to)
-    print(participant.sid)
-else:
-    participant = twilio_client.conversations \
-        .conversations(conversation_id) \
-        .participants \
-        .create(messaging_binding_address=phone_from, messaging_binding_proxy_address=phone_to)
-    print(participant.sid)
-
-# Send a message to the participant of the conversation
-message = twilio_client.conversations \
-    .conversations(conversation_id) \
-    .messages \
-    .create(
-         author='whatsapp:COURIER_WA_NUMBER',
-         body='Smart Teddy is being smart?'
-     )
-
+print(message.sid)  # send message
