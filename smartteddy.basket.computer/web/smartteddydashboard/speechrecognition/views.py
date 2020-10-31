68bb7326a7aea75138bd02add1a6ef0bf44a62b9
Huub van Holsteijn <hwpvanholsteijn@gmail.com>
2020-10-31 15:06:29 +0100

    Added view tests

--- smartteddy.basket.computer/web/smartteddydashboard/speechrecognition/views.py
+++ smartteddy.basket.computer/web/smartteddydashboard/speechrecognition/views.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import JSONParser
 from rest_framework import serializers
@@ -18,23 +19,15 @@ minimum_confidence = 0.0
 @parser_classes([JSONParser])
 def sentence(request):
     serializer = SentenceTextSerializer(data=request.data)
-    try:
-        print(serializer.is_valid())
-        print(serializer.errors)
-    except Exception as ex:
-        print(ex)
-    if serializer.is_valid():
-        try:
-            vosk_final_result = request.data
-            if environ('DEBUG'):
-                print(vosk_final_result)
-            for partial_results in vosk_final_result['result']:
-                if minimum_confidence <= partial_results['conf']:
-                    lem = SaidLemma(
-                        word_lemma=partial_results['word'],
-                        date_said=timezone.now(),
-                    )
-                    lem.save()
-        except Exception as ex:
-            logger.error(ex)
-    return Response({'received data': request.data})
+    if serializer.is_valid(raise_exception=True):
+        vosk_final_result = request.data
+        for partial_results in vosk_final_result['result']:
+            if minimum_confidence <= partial_results['conf']:
+                lem = SaidLemma(
+                    word_lemma=partial_results['word'],
+                    date_said=timezone.now(),
+                )
+                lem.save()
+        return Response(status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
