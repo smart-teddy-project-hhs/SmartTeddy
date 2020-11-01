7464f53cf509ba4ad9aa0d3bc24d001c5c9eb5b7
Jeoffrey-Oostrom <17068312@student.hhs.nl>
2020-11-01 12:24:25 +0000

    pipfile updated

--- smartteddy.basket.computer/service/usb_serial/main.py
+++ smartteddy.basket.computer/service/usb_serial/main.py
@@ -12,26 +12,27 @@ def main():
 
     if __name__ == 'usb_serial.main':
         with open("/home/vftp/admin/arduino_data.csv", 'w') as csvfile:
-            fieldnames = ["", "", "", "", "", "", "date"]
+            fieldnames = ["light sensor", "motion sensor", "sound sensor", "piezo sensor", "gas sensor value", "gas sensor ratio", "date"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
+
         while True:
             # Read serial and decode it from bytes to string
-            # try:
+            try:
                 # Receives sensor data in this order : light sensor data , motion sensor data , sound sensor data ,
                 # piezo sensor data , gas sensor value and gas sensor ratio.
-            arduinoData = ser.readline().decode()
-            # print(arduinoData)
-            date = time.asctime(time.localtime(time.time()))
+                arduinoData = ser.readline().decode()
+                # print(arduinoData)
+                date = time.asctime(time.localtime(time.time()))
 
-            if arduinoData:
-                print(arduinoData)
-                with open("/home/vftp/admin/arduino_data.csv", "a") as f:
-                    writer = csv.writer(f, delimiter=';')
-                    split = arduinoData.split(";")
-                    split.append(date)
-                    writer.writerow(split)
+                if arduinoData:
+                    print(arduinoData)
+                    with open("/home/vftp/admin/arduino_data.csv", "a") as f:
+                        writer = csv.writer(f, delimiter=';')
+                        split = arduinoData.split(";")
+                        split.append(date)
+                        writer.writerow(split)
 
-            # except:
-            #     print("An error has occured or program is stopped")
-            #     break
+            except:
+                print("An error has occured or program is stopped")
+                break
